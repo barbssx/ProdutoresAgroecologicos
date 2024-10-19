3ab9dfc2d327270289078734059dd30d507555f3
barbssx <bsthefaani@gmail.com>
2024-10-19 17:43:18 -0300

    Alterado URL api

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: 'https://produtoresagroecologicos-production.up.railway.app/api',
 });
 
 // Função de login
