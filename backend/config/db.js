ee7a73b2571138eeaad9fd6f4541e1a12ffbaf0b
barbssx <bsthefaani@gmail.com>
2024-10-20 11:23:10 -0300

    mongo_url

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,33 +4,24 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const connectDB = async () => {
-    try {
-        const uri = process.env.MONGO_PUBLIC_URL;
-        if (!uri) {
-            throw new Error('MONGO_PUBLIC_URL não está definido no arquivo .env');
-        }
-
-        console.log('Tentando conectar ao MongoDB...');
-
-        await mongoose.connect(uri, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useCreateIndex: true,
-            useFindAndModify: false 
-        });
-
-        console.log('Conectado ao MongoDB');
-    } catch (error) {
-        console.error('Erro ao conectar ao MongoDB:', error.message);
-        process.exit(1);
+  try {
+    const uri = process.env.MONGO_URL;
+    if (!uri) {
+      throw new Error('MONGO_URL não está definido no arquivo .env');
     }
-};
+    
+    console.log('Tentando conectar ao MongoDB com URI:', uri);
 
-// Para desconectar do banco de dados quando a aplicação for encerrada
-process.on('SIGINT', async () => {
-    await mongoose.connection.close();
-    console.log('Conexão ao MongoDB encerrada.');
-    process.exit(0);
-});
+    await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log('Conectado ao MongoDB');
+  } catch (error) {
+    console.error('Erro ao conectar ao MongoDB:', error.message);
+    process.exit(1);
+  }
+};
 
 module.exports = connectDB;
