a934aed3ca32dfe0d36e6eb2f8f00c73495f6861
barbssx <bsthefaani@gmail.com>
2024-10-20 12:10:36 -0300

    nova conexão

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,27 +1,22 @@
 const mongoose = require('mongoose');
-const dotenv = require('dotenv');
 
-dotenv.config();
+// Usando as variáveis de ambiente
+const mongoUser = process.env.MONGOUSER;
+const mongoPassword = process.env.MONGOPASSWORD;
+const mongoHost = process.env.MONGOHOST;
+const mongoPort = process.env.MONGOPORT;
+const databaseName = 'backend';
 
-const connectDB = async () => {
-  try {
-    const uri = process.env.MONGO_URI;
-    if (!uri) {
-      throw new Error('MONGO_URL não está definido no arquivo .env');
-    }
-    
-    console.log('Tentando conectar ao MongoDB com URI:', uri);
+// Construindo a URL de conexão
+const mongoUrl = `mongodb://${mongoUser}:${mongoPassword}@${mongoHost}:${mongoPort}/${databaseName}`;
 
-    await mongoose.connect(uri, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-
-    console.log('Conectado ao MongoDB');
-  } catch (error) {
-    console.error('Erro ao conectar ao MongoDB:', error.message);
-    process.exit(1);
-  }
-};
-
-module.exports = connectDB;
+mongoose.connect(mongoUrl, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => {
+  console.log('Conectado ao MongoDB');
+})
+.catch((err) => {
+  console.error('Erro ao conectar ao MongoDB:', err.message);
+});
