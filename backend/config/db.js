8900831341c28ce25d47f3f54d7491455588f466
barbssx <bsthefaani@gmail.com>
2024-10-19 21:27:52 -0300

    testemongo

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    const uri = process.env.MONGO_PUBLIC_URL;
+    const uri = process.env.MONGO_URI;
     if (!uri) {
       throw new Error('MONGO_URI não está definido no arquivo .env');
     }
