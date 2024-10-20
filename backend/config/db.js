6f7219e93041849116b0fb95808b0b2a492ab434
barbssx <bsthefaani@gmail.com>
2024-10-20 11:10:57 -0300

    Mongo conexão

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,24 +4,33 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const connectDB = async () => {
-  try {
-    const uri = process.env.MONGO_PUBLIC_URL;
-    if (!uri) {
-      throw new Error('MONGO_URI não está definido no arquivo .env');
+    try {
+        const uri = process.env.MONGO_URL;
+        if (!uri) {
+            throw new Error('MONGO_URL não está definido no arquivo .env');
+        }
+
+        console.log('Tentando conectar ao MongoDB...');
+
+        await mongoose.connect(uri, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true,
+            useFindAndModify: false 
+        });
+
+        console.log('Conectado ao MongoDB');
+    } catch (error) {
+        console.error('Erro ao conectar ao MongoDB:', error.message);
+        process.exit(1);
     }
-    
-    console.log('Tentando conectar ao MongoDB com URI:', uri);
-
-    await mongoose.connect(uri, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-
-    console.log('Conectado ao MongoDB');
-  } catch (error) {
-    console.error('Erro ao conectar ao MongoDB:', error.message);
-    process.exit(1);
-  }
 };
+
+// Para desconectar do banco de dados quando a aplicação for encerrada
+process.on('SIGINT', async () => {
+    await mongoose.connection.close();
+    console.log('Conexão ao MongoDB encerrada.');
+    process.exit(0);
+});
 
 module.exports = connectDB;
--- backend/models/Product.js
+++ backend/models/Product.js
@@ -1,13 +1,16 @@
 const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
-  name: { type: String, required: true }, // Nome do produto (obrigatório)
-  category: { type: String, required: true }, // Categoria do produto (obrigatório)
-  pricePerKg: { type: Number, required: true, min: 0 }, // Preço por kg (obrigatório)
-  biografia: { type: String }, // Biografia do produtor
-  available: { type: Boolean, default: true }, // Disponibilidade do produto
-  season: { type: String }, // Estação em que o produto está disponível
-  producer: { type: mongoose.Schema.Types.ObjectId, ref: 'Producer' }, // Referência ao modelo do Produtor
+  name: { type: String, required: true },
+  category: { type: String, required: true },
+  pricePerKg: { type: Number, required: true, min: [0, 'O preço por kg não pode ser negativo'] }, 
+  biografia: { type: String, maxlength: 500 }, 
+  available: { type: Boolean, default: true },
+  season: { 
+    type: String, 
+    enum: ['Anual', 'Verão', 'Outono', 'Inverno', 'Primavera']
+  }, // Estação em que o produto está disponível
+  producer: { type: mongoose.Schema.Types.ObjectId, ref: 'Producer' }, 
 });
 
 module.exports = mongoose.model('Product', productSchema);
